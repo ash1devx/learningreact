edffcd463da65230380652e99d1262d09c3db4c4
Shubhanshi Chaudhary <shubhanshi1105@gmail.com>
2025-05-06 07:05:14 +0530

    Learning React

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Greet from "./components/Greet" 
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import MainContent from "./components/MainContent"
+
+
+function App() {
+
+
+  return (
+    <div>
+     <Greet/>
+     <Header/>
+     <MainContent/>
+     <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent= () =>{
+    return (
+        <main>
+            <h2>Main Content</h2>
+            <p>Shubhanshi Sahil</p>
+        </main>
+    )
+}
+export default MainContent
